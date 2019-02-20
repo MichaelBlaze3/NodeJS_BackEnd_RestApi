52b64421a6dd8d69d65dbc81178da0281fcb21f3
Michael Blas <michaelblas@Michaels-MacBook-Pro.local>
2019-02-19 21:56:11 -0800

    My First commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Task = require('./api/models/todoListModel'),
+    bodyParser = require('body-parser');
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://super:super1@ds221435.mlab.com:21435/mk_todo_db');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// app.use(function(req, res){
+//     res.status(404).send({url:req.originalUrl + ' not found'}); 
+// });
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
